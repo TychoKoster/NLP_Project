b6db5c32194cc563647cb9b74c61e1268c58452a
TychoKoster <Tychokoster@hotmail.com>
2017-12-01 14:09:37 +0100

    bow update

--- bow_dialog.py
+++ bow_dialog.py
@@ -3,41 +3,57 @@ import numpy as np
 import sklearn
 from collections import Counter
 import pickle
+import string
 
 if __name__ == "__main__":
 	word_cnt = Counter()
 	visual_feat_mapping, data = rd.read_data()
+	translator = str.maketrans('', '', string.punctuation)
 	for sample in data.values():
 		caption = sample['caption']
 		dialog = sample['dialog']
-		img_list = sample['img_list']
-		target_id = sample['target_img_id']
+
 		# To add dialog as a feature.
-		# for word in dialog.split():
+		# for word in caption.split():
 		# 	word_cnt[word] += 1
 		for sentence in dialog:
-			for word in sentence[0].split():
+			sentence = sentence[0].translate(translator) 
+			for word in sentence.split():
 				word_cnt[word] += 1
 	word_list = list(word_cnt.keys())
 	all_bow_dialogs = []
 	for sample in data.values():
 		caption = sample['caption']
 		dialog = sample['dialog']
-		img_list = sample['img_list']
-		target_id = sample['target_img_id']
-		bow_dialog = []
-		# To add dialog as a feature.
-		# bow_caption = []
-		# for word in dialog.split():
-		# 	bow_caption.append(word_list.index(word))
+		dialog_list = []
 		for sentence in dialog:
-			bow_sentence = []
-			for word in sentence[0].split():
-				bow_sentence.append(word_list.index(word))
-			bow_dialog.append(bow_sentence)
-		# To add dialog as a feature.
-		# all_bow_captions.append(bow_caption)
-		all_bow_dialogs.append(bow_dialog)
-	with open("bow_dialog.txt", "wb") as fp:   #Pickling
-		pickle.dump(all_bow_dialogs, fp)
+			sentence_vec = np.zeros(len(word_list))
+			sentence = sentence[0].translate(translator)
+			for word in sentence.split():
+				word_index = word_list.index(word)
+				sentence_vec[word_index] += 1
+			dialog_list.append(sentence_vec)
+		all_bow_dialogs.append(dialog)
+
+
+	# for sample in data.values():
+	# 	caption = sample['caption']
+	# 	dialog = sample['dialog']
+	# 	img_list = sample['img_list']
+	# 	target_id = sample['target_img_id']
+	# 	bow_dialog = []
+	# 	# To add dialog as a feature.
+	# 	# bow_caption = []
+	# 	# for word in dialog.split():
+	# 	# 	bow_caption.append(word_list.index(word))
+	# 	for sentence in dialog:
+	# 		bow_sentence = []
+	# 		for word in sentence[0].split():
+	# 			bow_sentence.append(word_list.index(word))
+	# 		bow_dialog.append(bow_sentence)
+	# 	# To add dialog as a feature.
+	# 	# all_bow_captions.append(bow_caption)
+	# 	all_bow_dialogs.append(bow_dialog)
+	# with open("bow_dialog.txt", "wb") as fp:   #Pickling
+	# 	pickle.dump(all_bow_dialogs, fp)
 
--- read_data.py
+++ read_data.py
@@ -3,7 +3,7 @@ import json
 import numpy as np
 
 def read_data():
-	img_features = np.asarray(h5py.File('IR_image_features.h5', 'r')['img_features'])
+	# img_features = np.asarray(h5py.File('IR_image_features.h5', 'r')['img_features'])
 
 	with open('IR_img_features2id.json', 'r') as f:
 	     visual_feat_mapping = json.load(f)['IR_imgid2id']
