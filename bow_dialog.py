db6f5c44ee40371f6b1bf8d41c73fcfe7b071d37
jterstall <jeroen_t@hotmail.com>
2017-12-08 17:13:46 +0100

    Batches work?

--- bow_dialog.py
+++ bow_dialog.py
@@ -17,37 +17,31 @@ import time
 
 CONTEXT_SIZE = 1
 EMBEDDING_DIM = 300
-EPOCHS = 1
-BATCH_SIZE = 128
+ITERATIONS = 1
+BATCH_SIZE = 256
 MODEL_PATH = 'CBOW.pt'
 
 class CBOW(nn.Module):
-	def __init__(self, embedding_dim, vocab_size):
+	def __init__(self, embedding_dim, vocab_size, w2i):
 		super(CBOW, self).__init__()
 		self.embedding = nn.Embedding(vocab_size, embedding_dim, sparse=True)
-		self.embedding_2 = nn.Embedding(vocab_size, embedding_dim, sparse=True)
 		self.linear = nn.Linear(embedding_dim, vocab_size)
+		self.loss_function = nn.NLLLoss()
+		self.w2i = w2i
 
 	# Minimize (A*sum(q) + b), a linear combination with the sum of the embedded input
-	def forward(self, inputs, targets, vocab_batches):
+	def forward(self, inputs, target):
 		# Embed the context of a word and sum it into an embedded vector
 		embedded_input = self.embedding(inputs)
-		embedded_target = self.embedding_2(targets)
-		embedded_vocab = self.embedding_2(vocab_batches)
+		sum_embedding = torch.sum(embedded_input, dim=1)
+		output = self.linear(sum_embedding)
+		return self.compute_loss(F.log_softmax(output), target)
 
-		scores = embedded_target.bmm(embedded_input.transpose(1, 2)).squeeze(2)
-		norm_scores = embedded_vocab.bmm(embedded_input.transpose(1, 2)).squeeze(2)
-		
-		embedded = embedded_input.sum(0)
-		# Reshape to prevent input error when measuring loss
-		embedded = embedded.view(1,-1)
-		# Return the log probabilities
-		# return F.log_softmax(self.linear(embedded))	def nll_loss(self, scores, norm_scores):
-		return self.nll_loss(scores, norm_scores)
+	def compute_loss(self, probabilities, target):
+		return self.loss_function(probabilities, target)
 
-	def nll_loss(self, scores, norm_scores):
-		softmax = torch.exp(scores)/torch.sum(torch.exp(norm_scores),1).unsqueeze(1)
-		return -torch.mean(torch.log(softmax))
+	def embed_word(self, word):
+		return self.embedding(autograd.Variable(torch.LongTensor(self.w2i[word])))
 
 # Simple timer decorator
 def timer(func):
@@ -74,8 +68,6 @@ def retrieve_context_data(context_data, vocabulary, sentence):
 def process_data(data):
 	context_data = []
 	vocabulary = []
-	# translator = str.maketrans('', '', string.punctuation)
-	# word_cnt = Counter([word for sample in data.values() for sentence in sample['dialog'] for word in sentence[0].split()])
 	for sample in data.values():
 		dialog = sample['dialog']
 		for sentence in dialog:
@@ -92,70 +84,42 @@ def process_data(data):
 
 
 def context_to_index(context, w2i):
-	# indices = np.array([w2i[word] for word in context])
-	# context_vector = torch.from_numpy(indices).long()
-	# return context_vector
 	return np.array([w2i[word] for word in context])
 
 @timer
 def train_model_batches(context_data, vocab_size, w2i):
 	torch.manual_seed(1)
-	losses = []
+
 	# CBOW minimizes the negative log likelihood
-	loss_function = nn.NLLLoss()
-	model = CBOW(EMBEDDING_DIM, vocab_size)
+	model = CBOW(EMBEDDING_DIM, vocab_size, w2i)
 	optimizer = optim.SGD(model.parameters(), lr=0.001)
 
-	for _ in range(EPOCHS):
+	# context_data = context_data[:2000]
+	for iteration in range(ITERATIONS):
 		total_loss = torch.Tensor([0])
 		for i in range(0, len(context_data), BATCH_SIZE):
-			batch = context_data[i:i+BATCH_SIZE]
+			if i + BATCH_SIZE >= len(context_data):
+				batch = context_data[i:]
+			else:
+				batch = context_data[i:i+BATCH_SIZE]
+
 			context_vector = np.array([context_to_index(data_point[0], w2i) for data_point in batch])
 			target = np.array([np.array([w2i[data_point[1]]]) for data_point in batch])
 			
 			context_vector = autograd.Variable(torch.LongTensor(context_vector))
-			target = autograd.Variable(torch.LongTensor(target))
-			vocabs = autograd.Variable(torch.LongTensor(list(w2i.values()))).expand(context_vector.size(0), vocab_size)
-
+			target = autograd.Variable(torch.LongTensor(target)).squeeze()
 
 			model.zero_grad()
-			probabilities = model(context_vector, target, vocabs)
-			print(probabilities)
-			# loss = loss_function(probabilities, target[0])
-			# print(loss)
-			
-		# 	loss.backward()
-		# 	optimizer.step()
-		# 	total_loss += loss.data
-		# losses.append(total_loss)
-		# print("Total loss epoch {0}: {1}".format(iteration, total_loss))
-			break
-		break
+			optimizer.zero_grad()
 
+			loss = model(context_vector, target)
 
-	return model
-
-@timer
-def train_model(context_data, vocab_size, w2i):
-	losses = []
-	# CBOW minimizes the negative log likelihood
-	loss_function = nn.NLLLoss()
-	model = CBOW(CONTEXT_SIZE, EMBEDDING_DIM, vocab_size)
-	optimizer = optim.SGD(model.parameters(), lr=0.001)
-
-	for iteration in range(EPOCHS):
-		total_loss = torch.Tensor([0])
-		for context, target in context_data[:2000]:
-			context_vector = context_to_index(context, w2i)
-			model.zero_grad()
-			probabilities = model(context_vector)
-			target = autograd.Variable(torch.LongTensor([w2i[target]]))
-			loss = loss_function(probabilities, target)
 			loss.backward()
 			optimizer.step()
+			
 			total_loss += loss.data
-		losses.append(total_loss)
-		print("Total loss epoch {0}: {1}".format(iteration, total_loss))
+
+		print("Total loss iteration {0}: {1}".format(iteration, total_loss))
 
 	return model
 
@@ -167,7 +131,6 @@ def load_model(path):
 	return model
 
 def main():
-	data = list(range(450))
 	_, data_easy, data_hard = rd.read_data()
 	context_data, vocab_size, w2i = process_data(data_easy)
 	model = train_model_batches(context_data, vocab_size, w2i)
