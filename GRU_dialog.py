624637dd39e1116ae91705bb53890355bafd1066
jelmerta <jelmer_alphenaar@hotmail.com>
2017-12-12 13:31:38 +0100

    merge

--- GRU_dialog.py
+++ GRU_dialog.py
@@ -1,11 +1,10 @@
 # added GPU support because I thought training was slow, but GPU seems even slower?
 # Shouldn't we use cross entropy as loss?
 # Shuffle data?
-# Still errors...
 
 from collections import Counter
 
-import torch
+import torch.cuda
 import torch.autograd as autograd
 from torch.autograd import Variable
 import torch.nn as nn
@@ -20,12 +19,13 @@ import string
 import time
 
 NUM_LAYERS = 1
-CONTEXT_SIZE = 1
-EMBEDDING_DIM = 64
-EPOCHS = 10
+CONTEXT_SIZE = 3
+SEQ_LEN = CONTEXT_SIZE
+EMBEDDING_DIM = 128
+EPOCHS = 5
 MODEL_PATH = 'GRULM.pt'
-BATCH_SIZE = 64
-HIDDEN_SIZE = 128
+BATCH_SIZE = 256
+HIDDEN_SIZE = 1024
 LEARNING_RATE = 0.002
 
 class GRULM(nn.Module):
@@ -36,9 +36,8 @@ class GRULM(nn.Module):
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         #self.linear = nn.Linear(embedding_dim, vocab_size)
 
-        #self.gru = nn.GRU(EMBEDDING_FEAT*CONTEXT_SIZE*2, hidden_size)
         #self.gru = nn.GRU(embedding_dim, hidden_size, num_layers, batch_first=True) #batch_first=True?
-        self.lstm = nn.LSTM(embedding_dim, hidden_size, num_layers, batch_first=True) #batch_first=True?
+        self.lstm = nn.LSTM(embedding_dim, hidden_size, num_layers, batch_first=True, dropout=0.5, bidirectional=True) #batch_first=True?
         self.linear = nn.Linear(hidden_size, vocab_size)
         self.init_weights()
 
@@ -46,29 +45,29 @@ class GRULM(nn.Module):
         self.embedding.weight.data.uniform_(-0.1, 0.1)
         self.linear.bias.data.fill_(0)
         self.linear.weight.data.uniform_(-0.1, 0.1)
+        #hidden = (autograd.Variable(torch.randn(1, 1, 3)),
+          #autograd.Variable(torch.randn((1, 1, 3))))
 
     # Minimize (A*sum(q) + b), a linear combination with the sum of the embedded input
     def forward(self, inputs, hidden):
     # Embed the context of a word and sum it into an embedded vector
+        #print("input", inputs)
         embedded_input = self.embedding(inputs)
+        #print("e", embedded_input)
+        #embedded_input = embedded_input.view(BATCH_SIZE, CONTEXT_SIZE, -1)
 
         # Forward propagate GRU
-        #out, h = self.gru(embedded_input, hidden)
         out, h = self.lstm(embedded_input, hidden)
 
         # Reshape output
         out = out.contiguous().view(out.size(0)*out.size(1), out.size(2))
-        #rearranged = hn.view(hn.size()[1], hn.size(2))
-
+        #out = out.contiguous().view(out.size()[1], out.size(2))
         out = self.linear(out)
         return out, h
         #print(embedded_input)
         #embedded = nn.GRU()
         #GRU model here
 
-    #embedded = embedded_input.sum(0)
-    # Reshape to prevent input error when measuring loss
-    #embedded = embedded.view(1,-1)
     # Return the log probabilities
     #return F.log_softmax(self.linear(embedded))
 
@@ -82,119 +81,118 @@ def timer(func):
         return output
     return wrapper
 
-def retrieve_context_data(context_data, vocabulary, sentence):
+def retrieve_sequences(sequences, vocabulary, sentence):
     split_sentence = sentence.split()
     vocabulary.extend([word for word in sentence.split()])
     for i in range(CONTEXT_SIZE, len(split_sentence) - CONTEXT_SIZE):
-        context_lhs = split_sentence[i-CONTEXT_SIZE:i]
-        context_rhs = split_sentence[i+1:i+1+CONTEXT_SIZE]
-        context = np.concatenate((context_lhs, context_rhs))
-        target = split_sentence[i]
-        context_data.append((context, target))
-    return context_data, vocabulary
+        sequence = split_sentence[i-CONTEXT_SIZE:i+CONTEXT_SIZE+1]
+        sequences.append(sequence)
+    return sequences, vocabulary
 
 # Retrieve context data and the vocabulary of the dialog and captions
 def process_data(data):
-    context_data = []
+    sequences = []
     vocabulary = []
     # translator = str.maketrans('', '', string.punctuation)
     # word_cnt = Counter([word for sample in data.values() for sentence in sample['dialog'] for word in sentence[0].split()])
     for sample in data.values():
         dialog = sample['dialog']
         for sentence in dialog:
-            context_data, vocabulary = retrieve_context_data(context_data, vocabulary, sentence[0])
+            sentence_sequences = []
+            sentence_sequences, vocabulary = retrieve_sequences(sentence_sequences, vocabulary, sentence[0])
+            sequences.append(sentence_sequences)
         caption = sample['caption']
-        context_data, vocabulary = retrieve_context_data(context_data, vocabulary, caption)
+        sentence_sequences = []
+        sentence_sequences, vocabulary = retrieve_sequences(sentence_sequences, vocabulary, caption)
+        sequences.append(sentence_sequences)
 
     vocabulary = set(vocabulary)
     vocab_size = len(vocabulary)
 
     w2i = {word: i for i, word in enumerate(vocabulary)}
 
-    return context_data, vocab_size, w2i
+    return sequences, vocab_size, w2i
 
 def context_to_index(context, w2i):
-    indices = [w2i[word] for word in context]
-    #context_vector = torch.cuda.LongTensor(indices)
-    return indices
+    return np.array([w2i[word] for word in context])
+
+def make_data_points(sequences, w2i):
+    data_points = []
+    data_points_input = np.zeros((2*CONTEXT_SIZE+1), dtype=np.long)
+    data_points_output = np.zeros((2*CONTEXT_SIZE+1), dtype=np.long)
+    for sentence_sequences in sequences:
+        for i in range(len(sentence_sequences)-1):
+            data_point = []
+            context_vector = np.array(context_to_index(sentence_sequences[i], w2i))
+            target = np.array(context_to_index(sentence_sequences[i+1], w2i))
+            data_points_input = np.vstack((data_points_input, context_vector))
+            data_points_output = np.vstack((data_points_output, target))
+    data_points = [data_points_input, data_points_output]
+
+    return data_points
 
 @timer
-def train_model(context_data, vocab_size, w2i):
-    #torch.cuda.manual_seed(1)
-    torch.manual_seed(1)
-    losses = []
+def train_model(data_points, vocab_size, w2i):
+    torch.cuda.manual_seed(1)
+    #torch.manual_seed(1)
     # CBOW minimizes the negative log likelihood
 
     model = GRULM(CONTEXT_SIZE, EMBEDDING_DIM, vocab_size, HIDDEN_SIZE, NUM_LAYERS)
     loss_function = nn.CrossEntropyLoss()
+    #loss_function = nn.NLLLoss()
 
-    #model.cuda()
+    losses = []
+
+    model.cuda()
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
     def detach(states):
         return [state.detach() for state in states]
 
-    print(len(context_data))
+    #print(data_points[0].shape)
+    num_data_points = data_points[0].shape[0]
+    #print(len(data_points))
+    #print("n", num_data_points)
+    num_batches = num_data_points // BATCH_SIZE
 
-    # Should only run this part once, and already be in correct format
-    context_indices = np.zeros((CONTEXT_SIZE*2, len(context_data))) # this correct?
-    targets = np.zeros((1, len(context_data)))
-    count = 0
-    for context, target in context_data:
-        context_indices[:, count] = context_to_index(context, w2i)
-        targets[:, count] = [w2i[target]]
-        count += 1
-    print(context_indices.shape)
-    print("done")
+    #print("hi")
 
     for iteration in range(EPOCHS):
-        states = (Variable(torch.zeros(NUM_LAYERS, BATCH_SIZE, HIDDEN_SIZE)),
-              Variable(torch.zeros(NUM_LAYERS, BATCH_SIZE, HIDDEN_SIZE)))
-        #total_loss = torch.cuda.FloatTensor([0])
-        total_loss = torch.FloatTensor([0])
-    #count = 0
-        booltje = False
-        #for context, target in context_data:#
-        for i in range(0, context_indices.shape[1] - CONTEXT_SIZE*2, CONTEXT_SIZE*2): # Is this really correct? TODO
-            #context_batch = np.zeros((BATCH_SIZE, 1))
-            #context_batch = context_indices[:, t:t+b](
-            #context_batch = Variable(context_batch)
-            #inputs = torch.from_numpy(context_indices[:, i:i+BATCH_SIZE]).cuda()
-            inputs = torch.from_numpy(context_indices[:, i:i+CONTEXT_SIZE*2])
-            #inputs.type(torch.cuda.LongTensor)
-            inputs.type(torch.LongTensor)
-            inputs = Variable(inputs)
-            #targets = torch.from_numpy(targets[:, i:i+BATCH_SIZE]).cuda()
-            targets = torch.from_numpy(targets[:, i:i+CONTEXT_SIZE*2])
-            #targets.type(torch.cuda.LongTensor)
-            targets.type(torch.LongTensor)
-            targets = Variable(targets)
-            # if(not booltje):
-            #     print("c", context_vector)
-            #     print("s", context_vector.data.shape)
-            #     print("t", target)
-            #     print("s", target.data.shape)
-            #     booltje = True
+        #total_loss = torch.Tensor([0])
+
+        #print(iteration)
+
+        states = (Variable(torch.zeros(NUM_LAYERS, BATCH_SIZE, HIDDEN_SIZE)).cuda(),
+              Variable(torch.zeros(NUM_LAYERS, BATCH_SIZE, HIDDEN_SIZE).cuda()))
+
+        for i in range(0, num_data_points - BATCH_SIZE, BATCH_SIZE):# - CONTEXT_SIZE*2, CONTEXT_SIZE*2): # Is this really correct? TODO
+            batch_input = data_points[0][i:i+BATCH_SIZE]
+            batch_output = data_points[1][i:i+BATCH_SIZE]
+
+            inputs = Variable(torch.from_numpy(batch_input)).cuda()
+            #inputs = inputs.view(-1)
+            targets = Variable(torch.from_numpy(batch_output)).cuda()
+            targets = targets.view(-1)
 
             states = detach(states)
             outputs, states = model(inputs, states)
-            #word_embedding_prediction = model(inputs, Variable(torch.randn(NUM_LAYERS, BATCH_SIZE, HIDDEN_SIZE))) # I'm not sure what this output is meant to be?
-            #word_embedding_prediction = model(inputs)
-            #loss = loss_function(probabilities, targets) # Not sure what the loss function is
-            loss = loss_function(probabilities, targets.view(-1))
+            loss = loss_function(outputs, targets)
+
 
             loss.backward()
+            torch.nn.utils.clip_grad_norm(model.parameters(), 0.5)
+
             optimizer.step()
-            total_loss += loss.data
+            #total_loss += loss.data
 
-            step = (i+1) // CONTEXT_SIZE*2
-            if step % 100 == 0:
+            step = (i+1) // BATCH_SIZE
+            #print(step)
+            if step % BATCH_SIZE == 0:
                 print ('Epoch [%d/%d], Step[%d/%d], Loss: %.3f, Perplexity: %5.2f' %
-                       (iteration+1, EPOCHS, step, context_indices.shape[1] // BATCH_SIZE.exp(loss.data[0])))
+                       (iteration+1, EPOCHS, step, num_batches, loss.data[0], np.exp(loss.data[0])))
 
-        #    t += BATCH_SIZE
-        losses.append(total_loss)
-        print("Total loss epoch {0}: {1}".format(iteration, total_loss))
+        #losses.append(total_loss)11
+        #print("Total loss epoch {0}: {1}".format(iteration, total_loss))
 
     return model
 
@@ -207,8 +205,10 @@ def load_model(path):
 
 def main():
     _, data_easy, data_hard = rd.read_data()
-    context_data, vocab_size, w2i = process_data(data_easy)
-    small_set = context_data[:1000]
+    sequences, vocab_size, w2i = process_data(data_easy)
+    data_points = make_data_points(sequences[:8192], w2i)
+    #print(data_points[0:10])
+    small_set = data_points
     model = train_model(small_set, vocab_size, w2i)
     #save_model(model, MODEL_PATH)
     # model = load_model(MODEL_PATH)
